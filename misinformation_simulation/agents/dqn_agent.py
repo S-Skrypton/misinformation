c0e7990e2a2d1883537962c1b3d9bb6e8a09adc0
shihuis <shihuis@umich.edu>
2024-06-13 22:54:50 -0400

    fix some bug

--- misinformation_simulation/agents/dqn_agent.py
+++ misinformation_simulation/agents/dqn_agent.py
@@ -6,78 +6,6 @@ import torch.nn.functional as F
 from collections import deque
 import networkx as nx
 
-# The previously defined functions
-def create_social_network(num_nodes):
-    """Create social network graph."""
-    G = nx.DiGraph()
-    num_celebrities = min(int(0.04 * num_nodes), 100)
-    num_robots = min(int(0.05 * num_nodes), 500)
-    num_common = num_nodes - num_celebrities - num_robots
-    types = ['celebrity'] * num_celebrities + ['robot'] * num_robots + ['common'] * num_common
-    random.shuffle(types)
-    for i in range(num_nodes):
-        user_type = types[i]
-        repost_probability = 0.15 if user_type == 'celebrity' else 0.03 if user_type == 'common' else 0.01
-        G.add_node(i, followers=[], followings=[], type=user_type, repost_probability=repost_probability)
-    for i in G.nodes():
-        user_data = G.nodes[i]
-        followers = random.sample(list(G.nodes), min(int(random.uniform(0.2, 0.3) * num_nodes), 1) if user_data['type'] == 'celebrity' else min(int(random.uniform(0, 0.1) * num_nodes), 50))
-        followings = random.sample([n for n in G.nodes if n != i], min(int(random.uniform(0, 0.05) * num_nodes), 10) if user_data['type'] == 'celebrity' else min(int(random.uniform(0, 0.2) * num_nodes), 50))
-        for follower in followers:
-            if i != follower:
-                G.add_edge(follower, i)
-                G.nodes[i]['followers'].append(follower)
-        for following in followings:
-            if i != following:
-                G.add_edge(i, following)
-                G.nodes[following]['followers'].append(i)
-    return G
-
-def get_state(node_id, G):
-    node_data = G.nodes[node_id]
-    return [
-        node_data['type'],  # Type of the node
-        node_data['repost_probability'],  # Current repost probability
-        len(node_data['followers']) / len(G),  # Normalized number of followers
-        len(node_data['followings']) / len(G)  # Normalized number of followings
-    ]
-
-def apply_action(node_id, action, G):
-    """ Applies a given action to a node """
-    if action == 1:
-        # Block 5 posts
-        G.nodes[node_id]['blocked_posts'] = 5
-    elif action == 2:
-        # Do nothing
-        pass
-    elif action == 3:
-        # Label and reduce probability
-        G.nodes[node_id]['repost_probability'] *= 0.3
-    elif action == 4:
-        # Ban all in chain - requires identifying the chain first
-        pass
-
-def cost_of_action(action):
-    """Returns the cost of an action, exponentially increasing."""
-    return 2 ** action
-
-def cost_of_node_type(node_type):
-    """Returns the cost associated with the node's type."""
-    if node_type == 'celebrity':
-        return 10
-    elif node_type == 'common':
-        return 5
-    elif node_type == 'robot':
-        return 1
-    return 0
-
-def compute_reward(node_id, action, G):
-    """Computes the reward after an action."""
-    action_cost = cost_of_action(action)
-    node_type_cost = cost_of_node_type(G.nodes[node_id]['type'])
-    reward = -(action_cost + node_type_cost)
-    return reward
-
 # QNetwork and DQN classes
 class QNetwork(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim):
@@ -204,3 +132,7 @@ class DQN:
         # Decay epsilon
         if self.eps >= 0.01:
             self.eps *= 0.95
+            
+    def target_update(self):
+        # Update the target Q network (self.dqn_target) using the original Q network (self.dqn)
+        self.dqn_target.load_state_dict(self.dqn.state_dict())
